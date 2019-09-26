2c6eeff497893d709c0b5ea27ca05703ec115a16
silenziodilimbo <silenziodilimbo@gmail.com>
2019-09-26 20:57:21 +0800

    增加了一些定时器的内容

--- examples/asio/tutorial/timer4/timer.cc
+++ examples/asio/tutorial/timer4/timer.cc
@@ -10,6 +10,7 @@ class Printer : muduo::noncopyable
       count_(0)
   {
     // Note: loop.runEvery() is better for this use case.
+		// 给loop_添加了定时器, 1秒后调用print
     loop_->runAfter(1, std::bind(&Printer::print, this));
   }
 
@@ -20,11 +21,14 @@ class Printer : muduo::noncopyable
 
   void print()
   {
+		// loop_后的1秒调用
+		// 循环5次
     if (count_ < 5)
     {
       std::cout << count_ << "\n";
       ++count_;
-
+			// 再注册1秒定时器定时器
+			// 共计5次循环,5个定时器
       loop_->runAfter(1, std::bind(&Printer::print, this));
     }
     else
@@ -41,6 +45,7 @@ private:
 int main()
 {
   muduo::net::EventLoop loop;
+	// 进入构造
   Printer printer(&loop);
   loop.loop();
 }
