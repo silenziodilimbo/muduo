afe03719063000471fc593cf3aab24d4f7d69f88
silenziodilimbo <silenziodilimbo@gmail.com>
2019-10-24 16:13:12 +0800

    +EventLoopThread

--- muduo/net/EventLoopThread.cc
+++ muduo/net/EventLoopThread.cc
@@ -17,6 +17,7 @@ EventLoopThread::EventLoopThread(const ThreadInitCallback& cb,
                                  const string& name)
   : loop_(NULL),
     exiting_(false),
+    // 绑定thread_线程的运行函数
     thread_(std::bind(&EventLoopThread::threadFunc, this), name),
     mutex_(),
     cond_(mutex_),
@@ -31,21 +32,33 @@ EventLoopThread::~EventLoopThread()
   {
     // still a tiny chance to call destructed object, if threadFunc exits just now.
     // but when EventLoopThread destructs, usually programming is exiting anyway.
+    // 退出I/O线程，让I/O线程的loop循环退出，从而退出了I/O线程
     loop_->quit();
+    // 当前线程阻塞, 知道thread_终止了,才继续执行, 再析构当前线程
     thread_.join();
   }
 }
 
+// 启动成员thread_线程，该线程就成了I/O线程，内部调用thread_.start()
+// 返回新线程中EventLoop对象得地址, 用条件变量来等待线程得创建于运行
 EventLoop* EventLoopThread::startLoop()
 {
   assert(!thread_.started());
+  // 启动线程, 此时有两个线程在运行
+  // 执行threadFunc().
+  // 这里面会创建loop, loop.loop(),并把loop的引用给loop_
+  // 构造函数初始化列表中thread_(boost::bind(&EventLoopThread::threadFunc, this))
   thread_.start();
 
   EventLoop* loop = NULL;
   {
     MutexLockGuard lock(mutex_);
+    // 执行到这里, 会卡在while循环中, 等待并行线程thread_的执行
+    // 并行线程thread_会负责初始化loop_, 然后再执行下面的内容, 即返回这个loop
     while (loop_ == NULL)
     {
+      // startLoop()中wait了
+      // threadFunc()中notify了
       cond_.wait();
     }
     loop = loop_;
@@ -54,10 +67,16 @@ EventLoop* EventLoopThread::startLoop()
   return loop;
 }
 
+
+// thread_线程的运行函数
+// 启动loop循环
+// 该函数和上面的startLoop函数并发执行(两个线程)，所以需要上锁和condition
 void EventLoopThread::threadFunc()
 {
+  // 初始化一个loop
   EventLoop loop;
 
+  // 构造函数传递进来的，线程启动执行回调函数
   if (callback_)
   {
     callback_(&loop);
@@ -65,11 +84,21 @@ void EventLoopThread::threadFunc()
 
   {
     MutexLockGuard lock(mutex_);
+    // 一般情况是EventLoopThread对象先析构，析构函数调用loop_->quit() 使得loop.loop() 退出循环
+    // 这样threadFunc 退出，loop栈上对象析构，loop_ 指针失效，但此时已经不会再通过loop_ 访问loop，
+    // 故不会有问题
+    // 然后loop_指针指向了这个创建的栈上的对象，threadFunc退出之后，这个指针就失效了
     loop_ = &loop;
+    // startLoop()中wait了
+    // threadFunc()中notify了
+    // 该函数退出，意味着线程就推出了，EventLoopThread对象也就没有存在的价值了。但是muduo的EventLoopThread
+    // 实现为自动销毁的。一般loop函数退出整个程序就退出了，因而不会有什么大的问题
+    // 因为muduo库的线程池就是启动时分配，并没有释放。所以线程结束一般来说就是整个程序结束了
     cond_.notify();
   }
 
   loop.loop();
+  // 这是循环, 循环过程中下面是不会执行的
   //assert(exiting_);
   MutexLockGuard lock(mutex_);
   loop_ = NULL;
--- muduo/net/EventLoopThread.h
+++ muduo/net/EventLoopThread.h
@@ -22,6 +22,8 @@ namespace net
 
 class EventLoop;
 
+// 它创建一个线程
+// 在线程函数中. 创建了一个EventLoop对象, 并调用EventLoop::loop
 class EventLoopThread : noncopyable
 {
  public:
@@ -30,16 +32,22 @@ class EventLoopThread : noncopyable
   EventLoopThread(const ThreadInitCallback& cb = ThreadInitCallback(),
                   const string& name = string());
   ~EventLoopThread();
+
+  // 启动成员thread_线程，该线程就成了I/O线程，内部调用thread_.start()
   EventLoop* startLoop();
 
  private:
+   // 线程运行函数
   void threadFunc();
 
+  // 指向一个EventLoop对象，一个I/O线程有且只有一个EventLoop对象
   EventLoop* loop_ GUARDED_BY(mutex_);
   bool exiting_;
+  // 基于对象，包含了一个thread类对象
   Thread thread_;
   MutexLock mutex_;
   Condition cond_ GUARDED_BY(mutex_);
+  // 回调函数在EventLoop::loop事件循环之前被调用
   ThreadInitCallback callback_;
 };
 
